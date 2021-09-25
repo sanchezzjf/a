347f181af99c1f3c34b8006a9a24db085cf0ae32
Joao Filipe Ramos Ribeiro Sanchez <joao.sanchez@MacBook-Air-de-Joao.local>
2021-09-25 17:48:27 -0300

    modified some IDs and Classes to keep a pattern

--- WebAPP/public/app.js
+++ WebAPP/public/app.js
@@ -1,11 +1,13 @@
 import AppController from "./src/appController.js"
 import ConnectionManager from "./src/connectionManager.js"
 import ViewManager from "./src/viewManager.js"
+import DragAndDropManager from "./src/dragAndDropManager.js"
 
 const API_URL = 'https://0.0.0.0:3000'
 
 const appController = new AppController({
     viewManager: new ViewManager(),
+    dragAndDropManager: new DragAndDropManager(),
     connectionManager: new ConnectionManager({
         apiUrl: API_URL
     })
